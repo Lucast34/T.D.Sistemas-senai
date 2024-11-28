fbae60f4230cb27cf92edc60e02f5ff47183768d
Lucast34 <lucast34t@gmail.com>
2024-11-28 17:11:09 -0300

    exer-4

--- python/aula9/ex/exer-4.py
+++ python/aula9/ex/exer-4.py
@@ -3,3 +3,18 @@ Crie um programa que simule um caixa eletrônico. Peça ao usuário um valor a s
 Caso o usuário tente sacar mais do que o saldo ou insira um valor inválido, trate o erro de forma apropriada. 
 Garanta que o saldo atualizado seja sempre exibido no finally.
 '''
+
+conta = 45
+
+print('='*5,'CAIXA ELETRÔNICO','='*5)
+print('1-Retirar dineiro')
+print('2-Sair')
+
+escolhe = int(input("Escolha um opcão:\n>> "))
+
+match escolhe:
+    case 1:
+        print(deseja reitra quanto?)
+        print()
+
+print(conta)
