d6139bd21c8566449c6cc0596df5d030ba93126d
Lucast34 <lucast34t@gmail.com>
2024-12-02 16:14:29 -0300

    aula1 - modulos(falta implementar com hub)

--- python/aula1/index_1.py
+++ python/aula1/index_1.py
@@ -1,22 +1,24 @@
 from ex import modulo_aula1 as m
 
-
-while True:
-    escolha = int(input("escolha um dos exercicios"))
-    
-    match escolha:
-        case 1:
-            m.dicio()
-        case 2:
-            m.calc_nasc()
-        case _:
+def menu_1():
+    while True:
+        escolha = int(input("escolha um dos exercicios"))
+        
+        match escolha:
+            case 1:
+                m.dicio()
+            case 2:
+                m.calc_nasc()
+            case _:
+                print('Programa encerrado')
+                break
+        
+        esco_if = input('Deseja continuar?(s/n)\n>> ')
+        
+        if esco_if == 's':
+            print('Ok :)')
+        elif esco_if == 'n':
             print('Programa encerrado')
             break
-    
-    esco_if = input('Deseja continuar?(s/n)\n>> ')
-    
-    if esco_if == 's':
-        print('Ok :)')
-    elif esco_if == 'n':
-        print('Programa encerrado')
-        break
+
+menu_1()
