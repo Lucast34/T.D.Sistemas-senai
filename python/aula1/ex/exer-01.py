0dae84ed1ace1742f3119770df28565e09e84cca
Lucast34 <lucast34t@gmail.com>
2024-12-02 15:49:07 -0300

    backup

--- python/aula1/ex/exer-01.py
+++ python/aula1/ex/exer-01.py
@@ -2,15 +2,15 @@
 Escreva um algoritimo que leia nome, email e senha de um usuario e mostre apos o cadastro os dados salvos
 '''
 
+def dicio():
+    NOME = input("Digite o seu nome: ")
+    EMAIL = input("Digite o seu email: ")
+    SENHA = input("Senha: ")
 
-NOME = input("Digite o seu nome: ")
-EMAIL = input("Digite o seu email: ")
-SENHA = input("Senha: ")
+    pessoa={
+        'nome':NOME,
+        'email':EMAIL,
+        'senha':SENHA
+    }
 
-pessoa={
-    'nome':NOME,
-    'email':EMAIL,
-    'senha':SENHA
-}
-
-print(f'Cadastro realizado com sucesso:\n- Nome:{pessoa["nome"]}\n- E-mail:{pessoa["email"]}\n- Senha:{pessoa["senha"]}')
+    print(f'Cadastro realizado com sucesso:\n- Nome:{pessoa["nome"]}\n- E-mail:{pessoa["email"]}\n- Senha:{pessoa["senha"]}')
