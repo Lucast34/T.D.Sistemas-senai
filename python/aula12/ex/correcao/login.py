3c2b335f44be70b6bec95ad01d10ac5faf72d800
Lucast34 <lucast34t@gmail.com>
2024-12-04 10:16:12 -0300

    json exemplo

--- python/aula12/ex/correcao/login.py
+++ python/aula12/ex/correcao/login.py
@@ -1,19 +1,21 @@
 import json as j
 
 def login():
-    user = input('Digite o seu login >>')
-    usenha = input('Digite o seu senha >>')
+    while True:
+        user = input('Digite o seu login >>')
+        usenha = input('Digite o seu senha >>')
 
-    json_path = 'python/aula12/ex/correcao/dados.json'
+        json_path = 'python/aula12/ex/correcao/dados.json'
 
-    with open(json_path,'r',encoding='utf8') as leitura:
-        dados = j.load(leitura)
-        c_usuario = dados['user']
-        c_senha = dados['senha']
-        c_nome = dados['nome']
+        with open(json_path,'r',encoding='utf8') as leitura:
+            dados = j.load(leitura)
+            c_usuario = dados['user']
+            c_senha = dados['senha']
+            c_nome = dados['nome']
 
-    if c_usuario == user and c_senha == usenha:
-        print(f'Seja bem-vindo(a),{c_nome}')
-    else:
-        print('A senha ou usuario estão incorretos, tente novamente')
-    
+        if c_usuario == user and c_senha == usenha:
+            print(f'Seja bem-vindo(a),{c_nome}')
+            break
+        else:
+            print('A senha ou usuario estão incorretos, tente novamente')
+        
