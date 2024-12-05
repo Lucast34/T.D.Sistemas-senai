ad40ac6cf000c26587c398b4eec0eb3aeb8c5fc8
Lucast34 <lucast34t@gmail.com>
2024-12-05 14:21:36 -0300

    POO ex

--- python/aula14/ex/exer-01.py
+++ python/aula14/ex/exer-01.py
@@ -5,10 +5,24 @@ acelere o carro e também acrescente algo ao porta malas
 
 class Carro:
     def __init__(self,marca,modelo,cor) -> None:
-        self.marca = marca
-        self.modelo = modelo
+        self.__marca = marca
+        self.__modelo = modelo
         self.cor = cor
+        self.velocidade = 0
         self.portaMala = 0 
+    
+    def velo(self,km) -> None:
+        self.velocidade += km
+        print(f'O carro está a {self.velocidade}km/h')
 
-    def PortaMala(self) -> None:
-        pass
+    def portaMalapeso(self,kg) -> None:
+        self.portaMala += kg
+        print(f'O peso do porta mala e de {self.portaMala}kg')
+
+carro = Carro('Toyota','Trueno-AE86','Branco')
+
+print(carro.marca,carro.modelo)
+
+carro.portaMalapeso(60)
+
+carro.velo(90)
