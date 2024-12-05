e3bed3b4cdacd258655fa5e40a95909bdb92d8d0
Lucast34 <lucast34t@gmail.com>
2024-12-05 11:05:05 -0300

    POO pt 7

--- python/aula14/main3.py
+++ python/aula14/main3.py
@@ -6,13 +6,28 @@ class Carrinho:
     def __init__(self):
         self.produtos = []
     
-    def inserirProdutos(self,produto):
-        self,produto.append(produto)
+    def inserirProduto(self,*produtos):
+        for p in produtos:
+            self.produtos.append(p)
 
-class Produtos:
+    def listarProdutos(self):
+        for produto in self.produtos:
+            print(produto.nome)
+            print(produto.preco)
+
+class Produto:
     def __init__(self,nome,preco):
         self.nome = nome 
         self.preco = preco
 
 carrinho = Carrinho()
-p1 = Produtos('Relogio', 350)
+
+p1 = Produto('Relogio', 350)
+
+p2 = Produto('Hotwheels', 50)
+
+carrinho.inserirProduto(p1,p2)
+
+carrinho.listarProdutos()
+
+print(carrinho.__dict__)
