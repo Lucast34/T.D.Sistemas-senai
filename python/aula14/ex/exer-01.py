216f79abb9e22cc7ca08b2c0320ee4790934c1a7
L <lucast34t@gmail.com>
2024-12-05 19:36:44 -0300

    documentação exer-01

--- python/aula14/ex/exer-01.py
+++ python/aula14/ex/exer-01.py
@@ -4,18 +4,24 @@ acelere o carro e também acrescente algo ao porta malas
 """
 
 class Carro:
-    def __init__(self,marca,modelo,cor) -> None:
-        self.__marca = marca
-        self.__modelo = modelo
+    def __init__(self,marca,modelo,cor) -> None: # remover None
+        self.__marca = marca # private <- acho melhor trocar pro protected 
+        self.__modelo = modelo # private <- acho melhor trocar pro protected
         self.cor = cor
         self.velocidade = 0
         self.portaMala = 0 
+
+    """
+    #@ propeerty <- refatoração
+    """
     
-    def velo(self,km) -> None:
+    def velo(self,km) -> None:  # remover None
+        # adicionando velocidade
         self.velocidade += km
         print(f'O carro está a {self.velocidade}km/h')
 
-    def portaMalapeso(self,kg) -> None:
+    def portaMalapeso(self,kg) -> None: # remover None
+        # adicionando carga
         self.portaMala += kg
         print(f'O peso do porta mala e de {self.portaMala}kg')
 
@@ -23,6 +29,8 @@ carro = Carro('Toyota','Trueno-AE86','Branco')
 
 print(carro.marca,carro.modelo)
 
+# adicionando peso 
 carro.portaMalapeso(60)
 
+#adicionando velocida 
 carro.velo(90)
