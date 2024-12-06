23919f9849844a45c9b04704b96da2a0fedbb14a
Lucast34 <lucast34t@gmail.com>
2024-12-06 08:25:19 -0300

    mortorista e objetos

--- python/aula14/ex/exer-01.py
+++ python/aula14/ex/exer-01.py
@@ -4,33 +4,54 @@ acelere o carro e também acrescente algo ao porta malas
 """
 
 class Carro:
-    def __init__(self,marca,modelo,cor) -> None: # remover None
-        self.__marca = marca # private <- acho melhor trocar pro protected 
-        self.__modelo = modelo # private <- acho melhor trocar pro protected
+    def __init__(self,marca,modelo,cor): # feito
+        self._marca = marca # mudado
+        self._modelo = modelo # mudado
         self.cor = cor
+        self.ligado = False
         self.velocidade = 0
         self.portaMala = 0 
 
     """
     #@ propeerty <- refatoração
     """
-    
-    def velo(self,km) -> None:  # remover None
+    # Mais objtos
+    def ligar(self): # ligando 
+        self.ligado = True
+        print(f'O carro {self._modelo} etá ligado')
+
+    def desligar(self): # desligando
+        self.ligado = False
+        print(f'O carro {self._modelo} etá desligado')
+
+    def velo(self,km): # Feito
         # adicionando velocidade
         self.velocidade += km
         print(f'O carro está a {self.velocidade}km/h')
 
-    def portaMalapeso(self,kg) -> None: # remover None
+    def portaMalapeso(self,kg): # Feito
         # adicionando carga
         self.portaMala += kg
         print(f'O peso do porta mala e de {self.portaMala}kg')
 
+class Motorista:
+    def __init__(self)-> None:
+        self.nome 
+
+
+
 carro = Carro('Toyota','Trueno-AE86','Branco')
 
-print(carro.marca,carro.modelo)
+print(carro._marca,carro._modelo)
+
+carro.ligado()
+
+#adicionando velocida 
+carro.velo(90)
+
+carro.desligar()
 
 # adicionando peso 
 carro.portaMalapeso(60)
 
-#adicionando velocida 
-carro.velo(90)
+
