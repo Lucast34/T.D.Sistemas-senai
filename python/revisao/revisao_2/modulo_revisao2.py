e1eeb07ff2b6df9343f9a897e982d962cd1be230
Lucast34 <lucast34t@gmail.com>
2024-12-06 14:44:28 -0300

    revisão2

---
+++ python/revisao/revisao_2/modulo_revisao2.py
@@ -0,0 +1,10 @@
+'''
+1)Implemente um sistema de gerenciamento de estoque que inclua classes Produto, Estoque e métodos para adicionar, remover e verificar produtos.  
+
+2)Crie um sistema de gerenciamento de pedidos para um restaurante. Use classes Pedido, ItemPedido e Cardapio
+
+3)Crie uma classe Pessoa com os atributos nome e idade. Adicione um método para retornar a data de nascimento.  
+
+4)Crie uma classe Aluno que tenha os atributos nome, notas (uma lista) e métodos para calcular a média e verificar se o aluno foi aprovado (média >= 7). Todo aluno criado deverá ser adicionado a um Json
+'''
+
