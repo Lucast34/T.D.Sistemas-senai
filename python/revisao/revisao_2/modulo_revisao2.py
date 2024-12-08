ac79f149d0932b39ba5d11b35a12de2ca50fa34d
Lucast34 <lucast34t@gmail.com>
2024-12-08 20:13:58 -0300

    revisao.txt && def clear
    DEF clear import com OS para limpar o terminal ( em caso de loop de repetição)

--- python/revisao/revisao_2/modulo_revisao2.py
+++ python/revisao/revisao_2/modulo_revisao2.py
@@ -1,8 +1,20 @@
-def ex1():
-    class Estoque:
-        def __init__(self)-> None:
-            
+import os
+def apagar():
+    if os.plataform() == 'win32':
+        os.system('cls')
+    else:
+        os.system ('clear')
 
+
+
+
+def ex1():
+    dicio
+    
+    class Estoque:
+        def __init__(self,produto)-> None:
+            self.produto = produto
+            self.quantida = quantidade 
     class Produto:
         def __init__ (self) -> None:
             pass 
