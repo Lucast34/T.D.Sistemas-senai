ee61f628ffc1676d9c5e00a24257d58c65b8e9f9
Lucast34 <lucast34t@gmail.com>
2024-12-08 20:08:13 -0300

    EX e POO INIT

--- python/revisao/revisao_2/modulo_revisao2.py
+++ python/revisao/revisao_2/modulo_revisao2.py
@@ -1,12 +1,3 @@
-'''
-1)Implemente um sistema de gerenciamento de estoque que inclua classes Produto, Estoque e métodos para adicionar, remover e verificar produtos.  
-
-2)Crie um sistema de gerenciamento de pedidos para um restaurante. Use classes Pedido, ItemPedido e Cardapio
-
-3)Crie uma classe Pessoa com os atributos nome e idade. Adicione um método para retornar a data de nascimento.  
-
-4)Crie uma classe Aluno que tenha os atributos nome, notas (uma lista) e métodos para calcular a média e verificar se o aluno foi aprovado (média >= 7). Todo aluno criado deverá ser adicionado a um Json
-'''
 def ex1():
     class Estoque:
         def __init__(self)-> None:
@@ -15,5 +6,25 @@ def ex1():
     class Produto:
         def __init__ (self) -> None:
             pass 
+
+
+def ex2():
+    class Pedido:
+        def __init__(self) -> None:
+            ...
     
+    class ItemPedido:
+        def __init__(self) -> None:
+            ...
+    
+    class Cardapio:
+        def __init__ (self) -> None:
+            ...
+    ...
+
+def ex3():
+    ...
+
+def ex4():
+    ...
     
