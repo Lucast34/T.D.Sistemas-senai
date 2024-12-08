5dd77e3ada5643ba68301f542241067cb618c1b7
Lucast34 <lucast34t@gmail.com>
2024-12-08 20:01:10 -0300

    Delete

--- python/revisao/revisao-6.py
+++
@@ -1,7 +0,0 @@
-'''
-Mostra um número sucessor e antecessor ao que foi digitado
-'''
-
-num = int(input("Digite um número:\n>>"))
-
-print(f'O sucessor a ({num}) é ({num + 1}) e o antecessor é ({num - 1})')
